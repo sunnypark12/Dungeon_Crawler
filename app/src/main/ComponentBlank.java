1412bd0c1e2259faaf155f8e7de0f865e3356e95
sunnypark12 <143741023+sunnypark12@users.noreply.github.com>
2024-04-10 01:12:51 -0400

    Add files via upload

---
+++ app/src/main/ComponentBlank.java
@@ -0,0 +1,28 @@
+package com.example.cs2340s1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cs2340s1.navigation.FragmentHelper;
+
+public class ComponentBlank extends Fragment {
+    FragmentHelper fh;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fh = FragmentHelper.getInstance();
+    }
+}
